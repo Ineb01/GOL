e91dc4ac4bc56783f78c67e58392a8df9df2923f
Benjamin Bachmayr <Benjamin Bachmayr@Corsair380t>
2016-11-24 20:40:19 +0100

    creating print thread

--- GOL/main.c
+++ GOL/main.c
@@ -11,11 +11,28 @@
 #define XSIZE
 #define YSIZE
 
+void* PrintThread();
+
 int rgMain[XSIZE][YSIZE];
 
 int main(int argc, char** argv) {
-
+    
+    pthread_t threads[ 1 ];
+    int thread_args[ 1 ];
+    int result_code;
+    thread_args[ 0 ] = 0;
+    result_code = pthread_create(threads, NULL, PrintThread, thread_args);
     
     return (EXIT_SUCCESS);
 }
 
+void* PrintThread(){
+    int x, y;
+    for(x=0;x<XSIZE;x++){
+        for(y=0;y<YSIZE;y++){
+            if(rgMain[x][y])printf("#");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+}
