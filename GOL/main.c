213f232d6885aeff02b0aeddfe47705024d3b713
Benjamin Bachmayr <Benjamin Bachmayr@Corsair380t>
2016-11-24 20:45:57 +0100

    finealizing/testing print thread

--- GOL/main.c
+++ GOL/main.c
@@ -8,31 +8,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define XSIZE
-#define YSIZE
+#define XSIZE 3
+#define YSIZE 3
 
 void* PrintThread();
 
-int rgMain[XSIZE][YSIZE];
+int rgMain[XSIZE][YSIZE] = {1, 0, 0,
+                            0, 1, 0,
+                            1, 0, 1};
 
 int main(int argc, char** argv) {
-    
+
     pthread_t threads[ 1 ];
     int thread_args[ 1 ];
     int result_code;
     thread_args[ 0 ] = 0;
     result_code = pthread_create(threads, NULL, PrintThread, thread_args);
+
+    while(1){
+        sleep(10);
+    }
+    
     
     return (EXIT_SUCCESS);
 }
 
-void* PrintThread(){
+void* PrintThread() {
     int x, y;
-    for(x=0;x<XSIZE;x++){
-        for(y=0;y<YSIZE;y++){
-            if(rgMain[x][y])printf("#");
-            else printf(" ");
+    while (1) {
+        printf("\033[%d;%dH", 0, 0);
+        for (x = 0; x < XSIZE; x++) {
+            for (y = 0; y < YSIZE; y++) {
+                if (rgMain[x][y])printf("#");
+                else printf(" ");
+            }
+            printf("\n");
         }
-        printf("\n");
+        sleep(1);
     }
 }
