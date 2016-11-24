c2c6d9aa6456cf9865e8d3182891c58eea245af3
Benjamin Bachmayr <Benjamin Bachmayr@Corsair380t>
2016-11-24 20:18:56 +0100

    Creating project

---
+++ GOL/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Benjamin Bachmayr
+ *
+ * Created on 24. November 2016, 20:18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
